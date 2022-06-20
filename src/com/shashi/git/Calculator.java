7c864b60f603bcee150a99e164651cd3f3c2c0b0
shashirl9 <shashidhar.lucky9@gmail.com>
2022-06-20 22:12:26 +1000

    committed 2 methods (to perform sum and difference)

--- src/com/shashi/git/Calculator.java
+++ src/com/shashi/git/Calculator.java
@@ -1,4 +1,8 @@
 package com.shashi.git;
 
 public interface Calculator {
+
+    public Number sum(Number number1, Number number2);
+
+    public Number difference(Number number1, Number number2);
 }
