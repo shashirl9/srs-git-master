f335f5416966587868e0c77c1d25b2daf4e0fa85
shashirl9 <shashidhar.lucky9@gmail.com>
2022-06-20 22:17:27 +1000

    committed 2 methods (to perform product and division)

--- src/com/shashi/git/Calculator.java
+++ src/com/shashi/git/Calculator.java
@@ -5,4 +5,8 @@ public interface Calculator {
     public Number sum(Number number1, Number number2);
 
     public Number difference(Number number1, Number number2);
+
+    public Number product(Number number1, Number number2);
+
+    public Number division(Number number1, Number number2);
 }
